2aae3929e773a048abc374a813ec223be0e91713
Benjamin Zeiss <ben@zeiss.net>
2024-09-01 23:08:15 +0200

    secionlists are converted correctly now.

--- ConfigJSONPrinter.py
+++ ConfigJSONPrinter.py
@@ -7,7 +7,8 @@ from ConfigParserVisitor import ConfigParserVisitor
 class ConfigJSONPrinter(ConfigParserVisitor):
     def __init__(self):
         self.result = {}
-        self.current_section = self.result
+        self.current_context = [self.result]
+        self.current_section_name = None
 
     def visitConfig(self, ctx:ConfigParser.ConfigContext):
         self.visitChildren(ctx)
@@ -15,21 +16,46 @@ class ConfigJSONPrinter(ConfigParserVisitor):
 
     def visitSection(self, ctx:ConfigParser.SectionContext):
         section_name = ctx.IDENTIFIER().getText()
+        self.current_section_name = section_name
         new_section = {}
-        self.current_section[section_name] = new_section
-        previous_section = self.current_section
-        self.current_section = new_section
+        if isinstance(self.current_context[-1], list):
+            self.current_context[-1].append(new_section)
+        else:
+            self.current_context[-1][section_name] = new_section
+        self.current_context.append(new_section)
         self.visitChildren(ctx)
-        self.current_section = previous_section
+        self.current_context.pop()
+        self.current_section_name = None
 
     def visitSectionContent(self, ctx:ConfigParser.SectionContentContext):
         return self.visitChildren(ctx)
 
     def visitSectionList(self, ctx:ConfigParser.SectionListContext):
-        return self.visitChildren(ctx)
+        if self.current_section_name is None:
+            raise ValueError("SectionList encountered without a current section name")
+        section_list = []
+        current_dict = self.current_context[-1]
+        if self.current_section_name in current_dict:
+            if isinstance(current_dict[self.current_section_name], list):
+                section_list = current_dict[self.current_section_name]
+            else:
+                section_list = [current_dict[self.current_section_name]]
+                current_dict[self.current_section_name] = section_list
+        else:
+            current_dict[self.current_section_name] = section_list
+        self.current_context.append(section_list)
+        self.visitChildren(ctx)
+        self.current_context.pop()
 
     def visitSectionSingle(self, ctx:ConfigParser.SectionSingleContext):
-        return self.visitChildren(ctx)
+        if isinstance(self.current_context[-1], list):
+            new_section = {}
+            self.current_context[-1].append(new_section)
+            self.current_context.append(new_section)
+            self.visitChildren(ctx)
+            self.current_context.pop()
+        else:
+            self.visitChildren(ctx)
 
     def visitContent(self, ctx:ConfigParser.ContentContext):
         return self.visitChildren(ctx)
@@ -37,28 +63,25 @@ class ConfigJSONPrinter(ConfigParserVisitor):
     def visitVariable(self, ctx:ConfigParser.VariableContext):
         name = ctx.IDENTIFIER().getText()
         value = self.visit(ctx.value())
-        self.current_section[name] = value
+        self.current_context[-1][name] = value
 
     def visitVariableList(self, ctx:ConfigParser.VariableListContext):
         name = ctx.IDENTIFIER().getText()
         values = [self.visit(value) for value in ctx.value()]
-        self.current_section[name] = values
+        self.current_context[-1][name] = values
 
     def visitValue(self, ctx:ConfigParser.ValueContext):
         text = ctx.getText()
-        # Try to convert to appropriate type
         if ctx.NUMBER():
             return float(text) if '.' in text else int(text)
         elif ctx.BOOLEAN():
             return text.lower() == 'yes'
         elif ctx.TIME_WEEKS() or ctx.TIME_DAYS() or ctx.TIME_HOURS() or ctx.TIME_MINUTES() or ctx.TIME_SECONDS():
-            # Return as string for time values
             return text
         elif ctx.STRING():
             text = text[1:-1]  # Remove outer quotes
             text = re.sub(r'\\(.)', r'\1', text)  # Unescape all escaped characters
             return text
         else:
-            # For other types (IP addresses, MAC addresses, identifiers), return as is
             return text
 
